179357f9c536e210af3c13fcbdd4e8a95a19db14
kgcodex <hexcodex101@gmail.com>
2025-11-14 00:59:52 +0530

    Backend Setup Complete

---
+++ backend/connect_backend/settings/prod.py
@@ -0,0 +1,18 @@
+import os
+
+from dotenv import load_dotenv
+
+from .base import *
+
+load_dotenv()
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("PROD_DB_NAME"),
+        "USER": os.getenv("DB_USERNAME"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    }
+}
